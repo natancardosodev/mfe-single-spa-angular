52ec35f749982690ba8560aa9157c3816372f252
Vicente Correia <vicente@voxtecnologia.com.br>
2020-01-17 11:14:24 -0300

    [FIX] #9531 - Ajuste imports

--- src/app/components/input-date/input-date.module.ts
+++ src/app/components/input-date/input-date.module.ts
@@ -1,12 +1,13 @@
-import { ptBrLocale } from 'ngx-bootstrap/locale';
-import { locale } from './datepicker';
-import { defineLocale } from 'ngx-bootstrap/chronos';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { FormsModule } from '@angular/forms';
-import { InputDateComponent } from './input-date.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { ptBrLocale } from 'ngx-bootstrap/locale';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+
+import { locale } from './datepicker';
+import { InputDateComponent } from './input-date.component';
 
 defineLocale(locale, ptBrLocale);
 
