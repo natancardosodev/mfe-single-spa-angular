d02f5b6771589b6199890120def292c7ae80d9d5
Natan Cardoso <natan@voxtecnologia.com.br>
2021-01-27 17:59:20 -0300

    Lib UI Interno: substituição de Cards Component por app-list

--- src/app/core/components/card-pessoa/card-pessoa.module.ts
+++ src/app/core/components/card-pessoa/card-pessoa.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LibUIModule } from 'lib-ui-interno';
 import { CardPessoaComponent } from './card-pessoa.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
     declarations: [CardPessoaComponent],
-    imports: [CommonModule, SharedModule],
-    exports: [CardPessoaComponent]
+    imports: [CommonModule, SharedModule, LibUIModule],
+    exports: [CardPessoaComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CardPessoaModule {}
--- src/app/core/components/card-processo/card-processo.module.ts
+++ src/app/core/components/card-processo/card-processo.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LibUIModule } from 'lib-ui-interno';
 import { CardProcessoComponent } from './card-processo.component';
 
 @NgModule({
     declarations: [CardProcessoComponent],
-    imports: [CommonModule],
-    exports: [CardProcessoComponent]
+    imports: [CommonModule, LibUIModule],
+    exports: [CardProcessoComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CardProcessoModule {}
