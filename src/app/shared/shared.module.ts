9b7fffcf2f9d4941234bcff2e4194709ee1c4652
Natan Cardoso <natan@voxtecnologia.com.br>
2021-01-27 17:07:38 -0300

    remoção do alert component no shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MaskPipe } from './pipes/mask.pipe';
 import { CardsComponent } from './components/cards/cards.component';
-import { AlertModule } from './components/alert/alert.module';
 import { InputDateModule } from './components/input-date/input-date.module';
-import { AlertComponent } from './components/alert/alert.component';
 import { InputDateComponent } from './components/input-date/input-date.component';
 
 @NgModule({
     declarations: [MaskPipe, CardsComponent],
-    exports: [MaskPipe, CardsComponent, AlertComponent, InputDateComponent],
-    imports: [CommonModule, AlertModule, InputDateModule]
+    exports: [MaskPipe, CardsComponent, InputDateComponent],
+    imports: [CommonModule, InputDateModule]
 })
 export class SharedModule {}
