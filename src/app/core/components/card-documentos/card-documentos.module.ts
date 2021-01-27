5be9e56873f270e2280036e52cb19d9ba67a72b2
Natan Cardoso <natan@voxtecnologia.com.br>
2021-01-27 18:19:16 -0300

    Lib UI Interno: utilização de app-collapse

--- src/app/core/components/card-documentos/card-documentos.module.ts
+++ src/app/core/components/card-documentos/card-documentos.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LibUIModule } from 'lib-ui-interno';
 import { CardDocumentosComponent } from './card-documentos.component';
 
 @NgModule({
     declarations: [CardDocumentosComponent],
-    imports: [CommonModule],
-    exports: [CardDocumentosComponent]
+    imports: [CommonModule, LibUIModule],
+    exports: [CardDocumentosComponent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class CardDocumentosModule {}
