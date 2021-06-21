85f146577d82d6aadf53706f500db897e43f60c7
Guilherme Marques <guilherme.marques@voxtecnologia.com.br>
2021-06-21 10:26:36 -0300

    M229770 - Correção de arrow functions

--- src/app/core/mockys/interno/path-logo-mocky.ts
+++ src/app/core/mockys/interno/path-logo-mocky.ts
@@ -1,5 +1,6 @@
 export class PathLogoMocky {
     static data = {
-        url: 'https://www-storage.voxtecnologia.com.br/?m=sigfacil.pessoa&f=10459&i=7269d31766b0d1dec7500aa5cafa04b2.png'
+        url:
+            'https://www-storage.voxtecnologia.com.br/?m=sigfacil.pessoa&f=10459&i=7269d31766b0d1dec7500aa5cafa04b2.png'
     };
 }
